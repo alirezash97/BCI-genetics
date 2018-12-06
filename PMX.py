76dbb76bebf180704e80c50e19ef5d986c2c8d74
alireza shafaei <37047144+alirezash97@users.noreply.github.com>
2018-12-06 22:02:24 +0330

    Add files via upload

---
+++ PMX.py
@@ -0,0 +1,61 @@
+import random
+genes = int(input("please enter the number of genes : "))
+
+
+def pmx(parent1, parent2):
+    if len(parent1) == len(parent2):
+        genes = len(parent1)
+        points_2 = sorted(random.sample(range(1, genes), 2))
+        print("the selected points are :", points_2, "\n")
+        child1 = [0 for i in range(genes)]
+        child2 = [0 for j in range(genes)]
+        child1[points_2[0]:points_2[1]] = parent1[points_2[0]:points_2[1]]
+        child2[points_2[0]:points_2[1]] = parent2[points_2[0]:points_2[1]]
+        m = 0
+        for k in range(0, genes - points_2[1]):
+            if parent2[points_2[1] + k] in child1[points_2[0]:points_2[1]]:
+                pass
+            else:
+                child1[points_2[1] + m] = parent2[points_2[1] + k]
+                m += 1
+            if points_2[1] + m == genes:
+                m = m - genes
+
+        for k in range(0, points_2[1]):
+            if parent2[k] in child1[points_2[0]: points_2[1]]:
+                pass
+            elif points_2[1] + m != genes:
+                child1[points_2[1] + m] = parent2[k]
+                m += 1
+            if points_2[1] + m == genes:
+                m = m - genes
+
+        n = 0
+        for k in range(0, genes - points_2[1]):
+            if parent1[points_2[1] + k] in child2[points_2[0]: points_2[1]]:
+                pass
+            else:
+                child2[points_2[1] + n] = parent1[points_2[1] + k]
+                n += 1
+            if points_2[1] + n == genes:
+                n = n - genes
+
+        for k in range(0, points_2[1]):
+            if parent1[k] in child2[points_2[0]: points_2[1]]:
+                pass
+            elif points_2[1] + n != genes:
+                child2[points_2[1] + n] = parent1[k]
+                n += 1
+            if points_2[1] + n == genes:
+                n = n - genes
+        print("parent1 :", parent1)
+        print("parent2 :", parent2, "\n\n")
+        print("the selected points are :", points_2, "\n\n")
+        print("child1: ", child1)
+        print("child2: ", child2)
+        return child1, child2
+    else:
+        print("genes aren't equal")
+
+
+pmx(random.sample(range(0, genes), genes), random.sample(range(0, genes), genes))
