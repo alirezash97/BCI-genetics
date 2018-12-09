3b49aad4b6b750795acd2b20aec1d2fb348948e4
alireza shafaei <37047144+alirezash97@users.noreply.github.com>
2018-12-09 22:39:07 +0330

    Add files via upload

---
+++ Uniform mutation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def uniform(child):
+
+    genes = len(child)
+    mask = np.random.choice([0, 1], size=(genes,))
+    for i in range(0,genes):
+        if mask[i] == 1 :
+            child[i] = int(not(child[i]))
+        else:
+            pass
+    print("mask : ",mask)
+    return child
+
+print(uniform([1,0,1,1,1,0,0,1,1,1,1,1,0]))
