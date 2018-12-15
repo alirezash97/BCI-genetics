8ee1e0ecd434f72903bb5a622583cb9672a5115f
alireza shafaei <37047144+alirezash97@users.noreply.github.com>
2018-12-15 22:05:55 +0330

    Add files via upload

---
+++ Twors mutation.py
@@ -0,0 +1,7 @@
+import random
+def twors(child):
+    rand = random.sample(range(0,int(len(child)-1/2)),2)
+    child[rand[0]], child[rand[1]] = child[rand[1]], child[rand[0]]
+    return child
+print("child : ",[1,2,3,4,5,6,7,8,9,10,11,12,13])
+print("the mutated child is : ",twors([1,2,3,4,5,6,7,8,9,10,11,12,13]))
